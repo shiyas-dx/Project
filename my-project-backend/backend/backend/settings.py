c99bfe1910766cce736aa49001192d4d2c82462d
shiyas_dx <shiyasdev62@gmail.com>
2026-02-03 17:04:06 +0530

    add simplejwt

--- my-project-backend/backend/backend/settings.py
+++ my-project-backend/backend/backend/settings.py
@@ -141,8 +141,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 REST_FRAMEWORK = {
--- my-project-backend/backend/backend/urls.py
+++ my-project-backend/backend/backend/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -31,7 +34,10 @@ urlpatterns = [
     path("api/admin/", include("usersorders.urls")),
     path("api/admin/", include("users.urls")),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
