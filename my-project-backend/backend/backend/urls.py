4602588ab5ce2ce2196653b0ebe335d08d89eeee
shiyas_dx <shiyasdev62@gmail.com>
2026-02-04 09:40:56 +0530

    newone

--- my-project-backend/backend/backend/urls.py
+++ my-project-backend/backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path
 
 
 urlpatterns = [
@@ -37,7 +37,7 @@ urlpatterns = [
 ] 
 
 urlpatterns += [
-    url(r'^media/(?P<path>.*)$', serve, {
+    re_path(r'^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,
     }),
 ]
